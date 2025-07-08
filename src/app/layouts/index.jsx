2230ab8cf7db18feea85415ab9518a16cd9ac9b3
Shahin Ahmed <143713290+shahin715@users.noreply.github.com>
2025-07-08 22:03:10 +0600

    Revert "Banner"

--- src/app/layouts/index.jsx
+++ src/app/layouts/index.jsx
@@ -1,13 +1,11 @@
 import { Outlet } from 'react-router';
 import Header from '../../components/Header';
-import Banner from '../../components/Banner'
 import Footer from '../../components/Footer';
 
 function RootLayout() {
     return (
         <>
             <Header />
-            <Banner/>
             <main>
                 <Outlet />
             </main>
--- src/components/Banner/index.jsx
+++
@@ -1,7 +0,0 @@
-import BannerComponent from './banner';
-
-function Banner() {
-    return <BannerComponent />;
-}
-
-export default Banner;
